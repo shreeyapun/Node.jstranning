37d9d94d2bfc484677fba69f8ba678246fb759dc
shreeyapun <shreeyapun3@gmail.com>
2025-12-25 20:18:00 +0545

    async

--- async.js
+++ async.js
@@ -1,18 +1,18 @@
-// const fs=require("fs");
-// const path=require("path");
+const fs=require("fs");
+const path=require("path");
 
 
-// const fileName="shreeya.txt";
-// const filePath=path.join(__dirname,fileName);
+const fileName="shreeya.txt";
+const filePath=path.join(__dirname,fileName);
 
-// const writeFileExample= async ()=>{
-//     try{
-//         await fs.promises.writeFile(filePath,"Hello Diana!","utf-8");
-//     }catch(err){
-//         console.log(err);
-//     }
-// };
-// writeFileExample();
+const writeFileExample= async ()=>{
+    try{
+        await fs.promises.writeFile(filePath,"Hello Diana k gardai xau","utf-8");
+    }catch(err){
+        console.log(err);
+    }
+};
+writeFileExample();
 
 
 // async function writeFileExample(){
@@ -22,54 +22,57 @@
 // }
 // writeFileExample();
 
-
 //READ FILE
-const fs=require("fs");
-const path=require("path");
-const fileName="shreeya.txt";
-const filePath=path.join(__dirname,fileName);
-async function readFileExample(){
-    try{
-        const data= await fs.promises.readFile(filePath,"utf-8");
-        console.log(data);
-    }
-    catch(err){
-        console.log(err);
-    }
-};
-readFileExample();
+// const fs=require("fs");
+// const path=require("path");
+// const fileName="shreeya.txt";
+// const filePath=path.join(__dirname,fileName);
+// async function readFileExample(){
+//     try{
+//         const data= await fs.promises.readFile(filePath,"utf-8");
+//         console.log(data);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// };
+// readFileExample();
 
 
 
 // UPDATE FILE
-const fs1=require("fs");
-const path1=require("path");
-const fileName1="shreeya.txt";
-const filePath1=path1.join(__dirname,fileName1);
-async function updateFileExample(){
-    try{
-        await fs1.promises.appendFile(filePath1,"\n How are you?","utf-8");
-        console.log("file updated");
-    }
-    catch(err){
-        console.log(err);
-    }
-};
-updateFileExample();
+// const fs=require("fs");
+// const path1=require("path");
+// const fileName1="shreeya.txt";
+// const filePath1=path1.join(__dirname,fileName1);
+// async function updateFileExample(){
+//     try{
+//         await fs.promises.appendFile(filePath1,"file updated bayo hai ","utf-8");
+//         console.log("file updated");
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// };
+// updateFileExample();
 
 
 //DELETE FILE
-const fs2=require("fs");
-const path2=require("path");
-const fileName2="shreeya.txt";
-const filePath2=path2.join(__dirname,fileName2);
-async function  deleteFileExample (){
-    try{
-        await fs2.promises.unlink(filePath2);
-        console.log("file deleted");
-    }
-    catch(err){
-        console.log(err);
-    }
-};
-deleteFileExample();
+// const fs=require("fs");
+// const path2=require("path");
+// const fileName2="shreeya.txt";
+// const filePath2=path2.join(__dirname,fileName2);
+// async function  deleteFileExample (){
+//     try{
+//         await fs.promises.unlink(filePath2);
+//         console.log("file deleted");
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// };
+// deleteFileExample();
+
+
+
+// node --watch async.js
--- sync.js
+++ sync.js
@@ -1,11 +1,94 @@
-const fs =require("fs");
-const path =require("path");
-const fileName="crud.txt";
-const filePath=path.join(__dirname,fileName);
-console.log(filePath);
-fs.unlink(fileName,
-    (err)=>{
-        if(err) console.error(err);
-        else console.log("file  has been updated ");
-    }
-);
+// const fs =require("fs");
+// const path =require("path");
+// const fileName="crud.txt";
+// const filePath=path.join(__dirname,fileName);
+// console.log(filePath);
+// fs.unlink(fileName,
+//     (err)=>{
+//         if(err) console.error(err);
+//         else console.log("file  has been updated ");
+//     }
+// );
+
+
+
+
+// /  Create/writeFS.promises.writeFile().then().catch()
+
+// //write file
+// const fs= require ("fs/promises");
+// const path = require ("path");
+// const fileName="promise.txt";
+// const filePath = path.join(__dirname,fileName);
+// fs.writeFile(filePath,"Hello, Diana!","utf-8").then(
+//     (data)=> {console.log(data);}
+// ).catch(
+//     (error)=>{console.log(error);}
+// )
+
+// read file
+// const fs= require ("fs/promises");
+// const path = require ("path");
+
+// const fileName="promise.txt";
+// const filePath = path.join(__dirname,fileName);
+
+// fs.readFile(filePath,"utf-8").then(
+//     (data)=> {console.log(data);}
+// ).catch(
+//     (error)=>{console.log(error);}
+// )
+
+ //  update file
+
+// const fs= require ("fs/promises");
+// const path = require ("path");
+
+// const fileName="promise.txt";
+// const filePath = path.join(__dirname,fileName);
+
+// fs.appendFile(filePath,"\nHow are you?","utf-8").then(
+//     (data)=> {console.log(data);}
+// ).catch(
+//     (error)=>{console.log(error);}
+// )
+
+//  delete file
+// const fs= require ("fs/promises");
+// const path = require ("path");
+
+// const fileName="promise.txt";
+// const filePath = path.join(__dirname,fileName);
+
+// fs.unlink(filePath).then(
+//     (data)=> {console.log(udoooo);}
+// ).catch(
+//     (error)=>{console.log(error);}
+// )
+
+//reading files promise
+
+// const fs= require ("fs/promises");
+// const path = require ("path");
+
+// // const filePath = path.join(__dirname,"fileName");
+// const path1=__dirname;
+// fs.readdir(path1).then(
+//     (data)=> {console.log(data);}
+// ).catch(
+//     (error)=>{console.log(error);}
+// )
+
+
+
+// const fs= require("fs");
+// const path= require ("path");
+// const fileName="promise.txt";
+// const filePath=path.join(__dirname,fileName);
+
+// fs.readFile(filePath,"utf-8").then(
+//     (data)=>{console.log(data);}
+// ).catch(
+//     (error)=>{console.log(error);}
+// )
+
